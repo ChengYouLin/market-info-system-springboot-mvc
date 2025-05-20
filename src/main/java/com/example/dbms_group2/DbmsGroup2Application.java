317f52f4cb03e5ab8f3236187c002e563c264282
cyLin <111304019@nccu.edu.tw>
2025-05-20 12:31:45 +0800

    創建專案的所有基本內容和套件

---
+++ src/main/java/com/example/dbms_group2/DbmsGroup2Application.java
@@ -0,0 +1,13 @@
+package com.example.dbms_group2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsGroup2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbmsGroup2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbms_group2/DbmsGroup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbms_group2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsGroup2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
