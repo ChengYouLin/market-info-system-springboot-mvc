73f284d4aa237a9cddc333920287fd4512eb3fc1
cyLin <111304019@nccu.edu.tw>
2025-05-23 22:48:15 +0800

    finish vendor profile

--- src/main/java/com/example/dbms_group2/repository/VendorRepository.java
+++ src/main/java/com/example/dbms_group2/repository/VendorRepository.java
@@ -1,5 +1,6 @@
 package com.example.dbms_group2.repository;
 
+import com.example.dbms_group2.model.DTO.UserDTO;
 import com.example.dbms_group2.model.entity.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,8 @@ import java.util.List;
 public interface VendorRepository extends JpaRepository<Vendor,Integer> {
 
     List<Vendor> findByGmail(String email);
+
+    List<UserDTO> getVendorDetail(String email);
+
+    void updateVendorProfil(String name, String email);
 }
--- src/main/java/com/example/dbms_group2/service/VendorService.java
+++ src/main/java/com/example/dbms_group2/service/VendorService.java
@@ -3,9 +3,11 @@ package com.example.dbms_group2.service;
 import com.example.dbms_group2.model.DTO.AnnouncementDTO;
 import com.example.dbms_group2.model.DTO.ProductDTO;
 import com.example.dbms_group2.model.DTO.ProductVendorDTO;
+import com.example.dbms_group2.model.DTO.UserDTO;
 import com.example.dbms_group2.model.entity.Product;
 import com.example.dbms_group2.repository.ProductRepository;
 import com.example.dbms_group2.repository.UserQueryRepository;
+import com.example.dbms_group2.repository.VendorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,8 @@ public class VendorService {
     private ProductRepository productRepository;
     @Autowired
     private UserQueryRepository userQueryRepository;
+    @Autowired
+    private VendorRepository vendorRepository;
 
     public List<ProductVendorDTO> getFindAllProduct(String mail){
         return productRepository.findAllProduct(mail);
@@ -34,4 +38,11 @@ public class VendorService {
         productRepository.updateNewProduct(vendorId, name, category, price);
     }
 
+    public List<UserDTO> findGetVendorDetail(String email){
+        return vendorRepository.getVendorDetail(email);
+    }
+
+    public void findUpdateUserProfile(String name, String email){
+        vendorRepository.updateVendorProfil(name, email);
+    }
 }
